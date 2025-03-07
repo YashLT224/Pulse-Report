e75f30a290bdadba674ce8c63d5a5c05ec9bab04
Yash Verma <yashv@lambdatest.com>
2025-03-08 00:12:02 +0530

    redix store configured

---
+++ src/Screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+ 
+const Home = () => {
+  const data= useSelector((state:any)=>state.authReducer)
+  console.log(data)
+  return (
+    <div>
+      Home
+    </div>
+  )
+}
+
+export default Home
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { Amplify } from 'aws-amplify'
+import outputs from '../amplify_outputs.json'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './Redux/store'
+Amplify.configure(outputs)
 
-Amplify.configure(outputs);
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </>,
+)
