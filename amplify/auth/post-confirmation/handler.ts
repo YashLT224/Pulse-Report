19ab46bc923437ed8aafe89fc022f555b72fa752
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2025-03-08 18:27:21 +0530

    chore: add debug comments

--- amplify/auth/post-confirmation/handler.ts
+++ amplify/auth/post-confirmation/handler.ts
@@ -1,7 +1,12 @@
 import { PostConfirmationTriggerHandler } from "aws-lambda";
 
 export const handler: PostConfirmationTriggerHandler = async event => {
-    console.log("Amplify environment variables:", process.env);
+    console.log(
+        "Amplify environment variables:",
+        JSON.stringify(process.env, null, 2)
+    );
+
+    console.log("Post Confirmation event:", JSON.stringify(event, null, 2));
 
     // Create User Profile
     console.log("Creating user profile...");
