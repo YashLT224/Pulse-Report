aff9dc38f243cf1ee059c280e653d2e00dc9c1f3
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2025-03-09 17:26:14 +0530

    chore: clear user-profile on logout

--- src/Routes/routes.tsx
+++ src/Routes/routes.tsx
@@ -1,53 +1,57 @@
-import { useEffect } from "react";
-import { Routes, Route, Navigate } from 'react-router-dom'
-import { useAuthenticator } from "@aws-amplify/ui-react";
-import { generateClient } from "aws-amplify/data";
-import { useDispatch } from "react-redux";
-import Home from '../Screens/Home'
-import Staff from '../Screens/Staff'
-import ProtectedRoute from './protectedRoute'
-import { setUserProfile } from "../Redux/slices/userSlice";
-import { Schema } from "../../amplify/data/resource"
+import { useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useAuthenticator } from '@aws-amplify/ui-react';
+import { generateClient } from 'aws-amplify/data';
+import { useDispatch } from 'react-redux';
+import Home from '../Screens/Home';
+import Staff from '../Screens/Staff';
+import ProtectedRoute from './protectedRoute';
+import { setUserProfile } from '../Redux/slices/userSlice';
+import { Schema } from '../../amplify/data/resource';
 
- const client = generateClient<Schema>();
+const client = generateClient<Schema>();
 
 const AppRoutes = () => {
-   const { user } = useAuthenticator();
-   const dispatch = useDispatch(); // Get the dispatch function
+    const { user } = useAuthenticator();
+    const dispatch = useDispatch();
 
-    
     useEffect(() => {
         // Only run the effect if userId exists
         if (user?.userId) {
             const fetchUserProfile = async () => {
                 try {
-                    const result = await client.models.UserProfile.list({ 
-                        filter: { userId: { eq: user.userId } } 
+                    const result = await client.models.UserProfile.list({
+                        filter: { userId: { eq: user.userId } }
                     });
                     if (result.data && result.data.length > 0) {
                         dispatch(setUserProfile(result.data[0]));
                     }
                 } catch (error) {
-                    console.error("Error fetching user profile:", error);
+                    console.error('Error fetching user profile:', error);
                 }
             };
-            
+
             fetchUserProfile();
         }
-    }, [user?.userId,dispatch]);
+    }, [user?.userId, dispatch]);
 
-  return (   
-     <>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route 
-          path="/staff" 
-          element={<ProtectedRoute element={<Staff />} requiredRole="admin" />} 
-        />
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
-  </>
-  )
-}
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route
+                    path="/staff"
+                    element={
+                        <ProtectedRoute
+                            element={<Staff />}
+                            requiredRole="admin"
+                        />
+                    }
+                />
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </>
+    );
+};
 
-export default AppRoutes
+export default AppRoutes;
