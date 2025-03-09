ad6df297bc479b4c0919c4f09c4192d3ff983e26
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2025-03-09 17:17:26 +0530

    chore: minor auth fixes

--- src/components/AdminControls/index.tsx
+++ src/components/AdminControls/index.tsx
@@ -1,49 +1,58 @@
-import {Bar,Icon,Text,MenuItem} from './style.ts'
-import FormSvg from '../../assets/form.svg'
-import addUser from '../../assets/adduser.svg'
-import Approval from '../../assets/approval.svg'
-import List from '../../assets/list.svg'
-const Tiles=[
+import { useSelector } from 'react-redux';
+import { useAuthenticator } from '@aws-amplify/ui-react';
+import { Bar, Icon, Text, MenuItem } from './style.ts';
+import FormSvg from '../../assets/form.svg';
+import addUser from '../../assets/adduser.svg';
+import Approval from '../../assets/approval.svg';
+import List from '../../assets/list.svg';
+
+const Tiles = [
     {
-        name:'Form Access',
-        icon:FormSvg
+        name: 'Form Access',
+        icon: FormSvg
     },
     {
-        name:'Approval Requests',
-        icon:Approval,
+        name: 'Approval Requests',
+        icon: Approval
     },
     {
-        name:'Add User',
-        icon:addUser
+        name: 'Add User',
+        icon: addUser
     },
     {
-        name:'User List',
-        icon:List
+        name: 'User List',
+        icon: List
     }
-]
+];
 
-const Tile=({data})=>{
-    return <MenuItem>
-         <Icon src={data.icon} alt={data.name} />
-        <Text>{data.name}</Text>
-       
-    </MenuItem>
-}
-
+const Tile = ({ data }) => {
+    return (
+        <MenuItem>
+            <Icon src={data.icon} alt={data.name} />
+            <Text>{data.name}</Text>
+        </MenuItem>
+    );
+};
 
 const AdminControls = () => {
-    
-  return (
-    <div>
-      <Bar>
-          {Tiles.map((tile,index)=>{
-             return <Tile data={tile} key={index}/>
-          })
-          }
-      </Bar>
-    </div>
-  )
-}
+    const { user } = useAuthenticator();
+    const userProfile = useSelector(
+        (state: any) => state.authReducer.userProfile
+    );
 
-export default AdminControls
+    if (!user || !userProfile || userProfile.role !== 'admin') {
+        return null;
+    }
 
+    return (
+        <div>
+            <Bar>
+                {Tiles.map((tile, index) => {
+                    return <Tile data={tile} key={index} />;
+                })}
+            </Bar>
+        </div>
+    );
+};
+
+export default AdminControls;
