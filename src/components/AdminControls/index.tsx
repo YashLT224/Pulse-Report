d4e72417d26dea4bc7ca5f45e68cda92c382dbe1
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2025-03-09 18:45:43 +0530

    chore: add active marker in admin panel

--- src/components/AdminControls/index.tsx
+++ src/components/AdminControls/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useAuthenticator } from '@aws-amplify/ui-react';
 import { Bar, Icon, Text, MenuItem } from './style.ts';
@@ -6,29 +7,17 @@ import addUser from '../../assets/adduser.svg';
 import Approval from '../../assets/approval.svg';
 import List from '../../assets/list.svg';
 
-const Tiles = [
-    {
-        name: 'Form Access',
-        icon: FormSvg
-    },
-    {
-        name: 'Approval Requests',
-        icon: Approval
-    },
-    {
-        name: 'Add User',
-        icon: addUser
-    },
-    {
-        name: 'User List',
-        icon: List
-    }
+const tiles = [
+    { id: 0, name: 'Form Access', icon: FormSvg },
+    { id: 1, name: 'Approval Requests', icon: Approval },
+    { id: 2, name: 'Add User', icon: addUser },
+    { id: 3, name: 'User List', icon: List }
 ];
 
-const Tile = ({ data }) => {
+const Tile = ({ data, isActive, onClick }) => {
     return (
-        <MenuItem>
-            <Icon src={data.icon} alt={data.name} />
+        <MenuItem onClick={() => onClick(data.id)} active={isActive}>
+            <Icon src={data.icon} alt={data.name} active={isActive} />
             <Text>{data.name}</Text>
         </MenuItem>
     );
@@ -39,6 +28,7 @@ const AdminControls = () => {
     const userProfile = useSelector(
         (state: any) => state.authReducer.userProfile
     );
+    const [activeItem, setActiveItem] = useState(tiles[0].id); // Default active item
 
     if (!user || !userProfile || userProfile.role !== 'admin') {
         return null;
@@ -47,9 +37,14 @@ const AdminControls = () => {
     return (
         <div>
             <Bar>
-                {Tiles.map((tile, index) => {
-                    return <Tile data={tile} key={index} />;
-                })}
+                {tiles.map(tile => (
+                    <Tile
+                        key={tile.id}
+                        data={tile}
+                        isActive={tile.id === activeItem}
+                        onClick={setActiveItem}
+                    />
+                ))}
             </Bar>
         </div>
     );
