0773ed7fd0031efd3446810648c8d88adb6569ce
Yash Verma <yashv@lambdatest.com>
2025-03-14 23:49:07 +0530

    feat: reports containers created

--- src/Screens/Reports/Requirements/index.tsx
+++ src/Screens/Reports/Requirements/index.tsx
@@ -1,9 +1,139 @@
-const Requirements = () => {
-  return (
-    <div>
-      Requirements
-    </div>
-  )
-}
+import { useCallback, useState } from 'react';
+import { ulid } from 'ulid';
+import { Loader, Input, SelectField } from '@aws-amplify/ui-react';
+import { Schema } from '../../../../amplify/data/resource';
+import UserListItems from '../../../components/UserList';
+import useAuth from '../../../Hooks/useAuth';
+import { usePagination } from '../../../Hooks/usePagination';
+import PaginationControls from '../../../components/PaginationControls';
+import Modal from '../../../components/Modal';
+import { ModalButton, Heading } from '../../../style';
 
-export default Requirements
+const LIMIT = 10; // Number of items to display per page
+const heading = 'Requirements';
+const idField='formId';
+
+const Requirements = () => {
+    const { client } = useAuth();
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedItem, setSelectedItem] = useState<any>({});
+    const [isUpdateMode, setUpdateMode] = useState(false);
+
+    // Define columns for the People | Party list
+    const itemsColumns = [
+        {
+            key: 'createdAt',
+            header: 'Created At'
+            // render: (item: Entity) => new Date(item.createdAt).toLocaleString()
+        },
+        {
+            key: 'demandFrom',
+            header: 'Demand From'
+            // render: (item: Entity) => new Date(item.createdAt).toLocaleString()
+        },
+        {
+            key: 'dueDate',
+            header: 'Deadline'
+            // render: (item: Entity) => new Date(item.createdAt).toLocaleString()
+        },
+        {
+            key: 'description',
+            header: 'Description'
+            // render: (item: Entity) => new Date(item.createdAt).toLocaleString()
+        },
+        {
+            key: 'remarks',
+            header: 'Remarks'
+            // render: (item: Entity) => new Date(item.createdAt).toLocaleString()
+        }
+    ];
+
+    // fetch function for usePagination
+    const fetchEntity = useCallback(
+        async (limit: number, token?: string) => {
+            // const params: any = {
+            //     entityType,
+            //     nextToken: token,
+            //     limit,
+            //     sortDirection: 'ASC'
+            // };
+            // const response = await model[listfuncName](params);
+
+            return {
+                data:[],
+                nextToken:  null
+            };
+        },
+        []
+    );
+
+    // Use the usePagination hook
+    const {
+        items,
+        isLoading,
+        hasNext,
+        hasPrevious,
+        goToNext,
+        goToPrevious,
+        initiateLoding,
+        updateItem,
+        refreshList
+    } = usePagination<Entity>({
+        limit: LIMIT,
+        fetchFn: fetchEntity,
+        idField
+    });
+
+    const addNewItemHandler = () => {
+        setUpdateMode(false);
+        setIsModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleEdit = () => {};
+
+    return (
+        <>
+            <div style={{ position: 'relative' }}>
+                {/* Loader overlay */}
+                {isLoading && (
+                    <div
+                        style={{
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            right: 0,
+                            bottom: 0,
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            background: 'rgba(255, 255, 255, 0.7)',
+                            zIndex: 10
+                        }}
+                    >
+                        <Loader
+                            height={'80px'}
+                            size="large"
+                            emptyColor="#007aff"
+                            filledColor="white"
+                        />
+                    </div>
+                )}
+                <UserListItems<Entity>
+                    heading={heading}
+                    items={items}
+                    columns={itemsColumns}
+                    addNewEntryAccess={true}
+                    addNewItemHandler={addNewItemHandler}
+                    handleEdit={handleEdit}
+                />
+            </div>
+        </>
+    );
+};
+
+export default Requirements;
