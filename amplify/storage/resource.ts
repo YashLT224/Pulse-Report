c5b4efbd14a9c0fd843030c302cafdcdd9d91529
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2025-03-16 22:05:03 +0530

    feat: add access control for vehicleInsurance forms in storage configuration

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,5 +1,12 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-    name: 'amplifyPulseReport'
+    name: 'amplifyPulseReport',
+    access: allow => ({
+        'forms/vehicleInsurance/{entity_id}/*': [
+            allow.groups(['ADMINS']).to(['read', 'write', 'delete']),
+            allow.entity('identity').to(['read', 'write', 'delete']),
+            allow.authenticated.to(['read'])
+        ]
+    })
 });
